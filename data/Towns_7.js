70f9c0b35079c647a2417478e49a308d6fe9fd5a
teamdhaksha <63536148+teamdhaksha@users.noreply.github.com>
2021-01-09 13:13:15 +0530

    Add files via upload

---
+++ data/Towns_7.js
@@ -0,0 +1,12 @@
+var json_Towns_7 = {
+"type": "FeatureCollection",
+"name": "Towns_7",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NAME": "Sivaganga" }, "geometry": { "type": "Point", "coordinates": [ 78.484708272088653, 9.850194849584456, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Kalayarkoil" }, "geometry": { "type": "Point", "coordinates": [ 78.628563501118037, 9.846798993764621, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Manamadurai" }, "geometry": { "type": "Point", "coordinates": [ 78.456756261515366, 9.692736641048278, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Karaikudi" }, "geometry": { "type": "Point", "coordinates": [ 78.7774504065749, 10.07650434318152, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Singampunari" }, "geometry": { "type": "Point", "coordinates": [ 78.421696587912365, 10.185195409547809, -999999.0 ] } }
+]
+}
